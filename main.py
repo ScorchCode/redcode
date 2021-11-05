2db457701604258e44e879c44513858f4c4c39ff
Henning <brink_hb@freenet.de>
2021-11-05 17:13:43 +0100

    Add copy to clipboard.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import frames
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Reddit Codeblock Poster")
+        self.title("Reddit Codeblock Pasting Crutch")
 
         self.editor = frames.Editor(parent=self)
         self.button = frames.Buttons(parent=self)
@@ -19,7 +19,22 @@ class App(tk.Tk):
         self.button.grid(sticky=tk.EW)
         self.editor.grid(sticky=tk.NSEW)
 
-        self.editor.text.insert(1.0, "text")
+    def clear(self):
+        self.editor.text.delete(1.0, "end")
+
+    def done(self):
+        indent = "    "
+        codeblock = indent
+        for c in self.editor.text.get(1.0, "end-1c"):
+            codeblock += c
+            if c == "\n":
+                codeblock += indent
+
+        self.clipboard_clear()
+        self.clipboard_append(codeblock)
+
+    def from_clipboard(self):
+        pass
 
 
 if __name__ == "__main__":
