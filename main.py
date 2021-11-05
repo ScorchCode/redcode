5b71d18248e4e3cfa079c1811178b49377490e55
Henning <brink_hb@freenet.de>
2021-11-05 19:35:23 +0100

    Move frames to main.py, remove frames.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from pathlib import Path
 import tkinter as tk
-from tkinter import messagebox, filedialog
-
-import frames
+from tkinter import ttk, messagebox, filedialog
 
 
 class App(tk.Tk):
     """
-    A Python + Tkinter app that adds 4 Spaces before every line.
+    A Python + Tkinter app that adds 4 spaces before every line.
 
     Reddit's Deluxe Editor is buggy when pasting.
     When pasting code in Markdown modus, a codeblock needs to have
-    4 space preceeding every line.
+    4 spaces preceding every line.
 
     Edit your code here and click "Done".
     Your code, including the extra spaces, gets copied to the clipboard
@@ -21,14 +19,14 @@ class App(tk.Tk):
         super().__init__()
         self.title("Reddit Codeblock Pasting Crutch")
 
-        self.editor = frames.Editor(parent=self)
-        self.button = frames.Buttons(parent=self)
+        self.editor = Editor(parent=self)
+        self.button = Buttons(parent=self)
 
         self.button.pack(expand=True, fill=tk.X)
         self.editor.pack(expand=True, fill=tk.BOTH)
 
     def clear(self):
-        """Clear text from editor."""
+        """Delete all text from editor."""
         self.editor.text.delete(1.0, "end")
 
     def done(self):
@@ -56,6 +54,44 @@ class App(tk.Tk):
         ))
         self.clear()
         self.editor.text.insert(1.0, textfile.read_text())
+
+
+class Buttons(ttk.Frame):
+    """A tkinter.Frame with Buttons."""
+    def __init__(self, parent):
+        super().__init__(master=parent)
+        self.open = ttk.Button(master=self, text="Open", command=parent.open_file)
+        self.clear = ttk.Button(master=self, text="Clear", command=parent.clear)
+        self.done = ttk.Button(master=self, text="Done", command=parent.done)
+
+        self.open.pack(side=tk.LEFT)
+        self.clear.pack(side=tk.LEFT)
+        self.done.pack(side=tk.LEFT)
+
+
+class Editor(ttk.Frame):
+    """A tkinter.Frame showing a text widget with scrollbars."""
+    def __init__(self, parent):
+        super().__init__(master=parent)
+        self.text = tk.Text(
+            self,
+            width=80, height=20,
+            wrap=tk.NONE,
+            padx=10, pady=5,
+            font="TkFixedFont"
+        )
+        self.scb_vertical = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.text.yview)
+        self.scb_horizontal = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=self.text.xview)
+
+        self.text.configure(yscrollcommand=self.scb_vertical.set)
+        self.text.configure(xscrollcommand=self.scb_horizontal.set)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.text.grid(row=0, column=0, sticky=tk.NSEW)
+        self.scb_vertical.grid(row=0, column=1, sticky=tk.NS)
+        self.scb_horizontal.grid(row=1, column=0, sticky=tk.EW)
 
 
 if __name__ == "__main__":
